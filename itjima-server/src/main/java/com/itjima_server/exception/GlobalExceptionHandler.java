0b23983136f4f334c44d5cd4e40fc3345f8bcdc6
Rege-97 <cogus1027@gmail.com>
2025-08-19 15:59:46 +0900

    :sparkles: feat: Valid 검증 예외핸들러 추가

--- itjima-server/src/main/java/com/itjima_server/exception/GlobalExceptionHandler.java
+++ itjima-server/src/main/java/com/itjima_server/exception/GlobalExceptionHandler.java
@@ -1,15 +1,31 @@
 package com.itjima_server.exception;
 
 import com.itjima_server.common.ApiResponseDTO;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+
+        String errors = fieldErrors.stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponseDTO.error(HttpStatus.BAD_REQUEST.value(), errors));
+    }
 
     @ExceptionHandler(DuplicateUserFieldException.class)
     public ResponseEntity<?> handleDuplicateEmailException(DuplicateUserFieldException e) {
