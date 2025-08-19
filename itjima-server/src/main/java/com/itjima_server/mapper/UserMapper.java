d67dc5a4e9087139db05dbe42954f2961738f04a
Rege-97 <cogus1027@gmail.com>
2025-08-19 15:19:15 +0900

    :sparkles: feat: 이메일 검증용 조회 매퍼 작성

--- itjima-server/src/main/java/com/itjima_server/mapper/UserMapper.java
+++ itjima-server/src/main/java/com/itjima_server/mapper/UserMapper.java
@@ -1,10 +1,14 @@
 package com.itjima_server.mapper;
 
+import com.itjima_server.domain.Users;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.security.core.userdetails.User;
 
 @Mapper
 public interface UserMapper {
+
+    Users findByEmail(@Param("email") String email);
 
     int insert(User user);
 }
