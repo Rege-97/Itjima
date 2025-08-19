e182e5426dd94efa6288912cf98cc77a5cecfee7
Rege-97 <cogus1027@gmail.com>
2025-08-19 15:36:22 +0900

    :sparkles: feat: 공통 응답 DTO 생성

---
+++ itjima-server/src/main/java/com/itjima_server/common/ApiResponseDTO.java
@@ -0,0 +1,23 @@
+package com.itjima_server.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponseDTO<T> {
+
+    private int status;
+    private String message;
+    private T data;
+
+    // 성공 응답
+    public static <T> ApiResponseDTO<T> success(int status, String message, T data) {
+        return new ApiResponseDTO<>(status, message, data);
+    }
+
+    // 실패 응답
+    public static <T> ApiResponseDTO<T> error(int status, String message) {
+        return new ApiResponseDTO<>(status, message, null);
+    }
+}
