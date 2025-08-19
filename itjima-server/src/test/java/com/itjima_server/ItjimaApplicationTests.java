28db445a8c716ed7b11f5dffdcc5c4ae77261c46
Rege-97 <cogus1027@gmail.com>
2025-08-19 13:31:01 +0900

    🎉 Init: 백엔드 프로젝트 생성

---
+++ itjima-server/src/test/java/com/itjima_server/ItjimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itjima_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItjimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
