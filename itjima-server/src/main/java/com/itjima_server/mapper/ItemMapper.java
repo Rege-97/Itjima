a696ef24c1b5430b3105ecc51122a4e6b23502e1
Rege-97 <cogus1027@gmail.com>
2025-08-20 15:50:23 +0900

    :sparkles: feat: 물품 수정을 위한 매퍼 작성

--- itjima-server/src/main/java/com/itjima_server/mapper/ItemMapper.java
+++ itjima-server/src/main/java/com/itjima_server/mapper/ItemMapper.java
@@ -2,9 +2,14 @@ package com.itjima_server.mapper;
 
 import com.itjima_server.domain.Item;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface ItemMapper {
 
     int insert(Item item);
+
+    boolean existsById(@Param("id") Long id);
+
+    int update(Item item);
 }
