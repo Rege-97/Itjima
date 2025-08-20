7f7af76d7ef0602663a9c14afa7f47ec9dcf8cf7
Rege-97 <cogus1027@gmail.com>
2025-08-20 14:46:58 +0900

    :sparkles: feat: Items 테이블 도메인 생성

---
+++ itjima-server/src/main/java/com/itjima_server/domain/Item.java
@@ -0,0 +1,17 @@
+package com.itjima_server.domain;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Item {
+
+    private long id;
+    private long userId;
+    private ItemType type;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+}
