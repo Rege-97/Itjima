db89f3c15a315211d546cfe8163320e2c6c2df40
Rege-97 <cogus1027@gmail.com>
2025-08-22 18:43:29 +0900

    :sparkles: feat: 대여 수락 및 거절을 위한 매퍼 작성

--- itjima-server/src/main/java/com/itjima_server/mapper/AgreementMapper.java
+++ itjima-server/src/main/java/com/itjima_server/mapper/AgreementMapper.java
@@ -1,10 +1,16 @@
 package com.itjima_server.mapper;
 
 import com.itjima_server.domain.Agreement;
+import com.itjima_server.domain.AgreementStatus;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface AgreementMapper {
 
     int insert(Agreement agreement);
+
+    Agreement findById(@Param("id") int id);
+
+    int updateStatusById(@Param("status") AgreementStatus status);
 }
--- itjima-server/src/main/java/com/itjima_server/mapper/AgreementPartyMapper.java
+++ itjima-server/src/main/java/com/itjima_server/mapper/AgreementPartyMapper.java
@@ -1,10 +1,17 @@
 package com.itjima_server.mapper;
 
 import com.itjima_server.domain.AgreementParty;
+import java.time.LocalDateTime;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface AgreementPartyMapper {
 
     int insert(AgreementParty agreementParty);
+
+    AgreementParty findByAgreementIdAndRole(@Param("agreementId") int agreementId,
+            @Param("Role") String role);
+
+    int updateConfirmedAtById(@Param("id") int id, @Param("confirmedAt") LocalDateTime confirmedAt);
 }
