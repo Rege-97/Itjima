944b48c96c3034ef2260f2359801c2dbdd7d0b3d
Rege-97 <cogus1027@gmail.com>
2025-08-24 15:42:02 +0900

    :sparkles: feat: 대여 수락 API 컨트롤러 구현

--- itjima-server/src/main/java/com/itjima_server/controller/AgreementController.java
+++ itjima-server/src/main/java/com/itjima_server/controller/AgreementController.java
@@ -9,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,14 @@ public class AgreementController {
         AgreementResponseDTO res = agreementService.create(user.getId(), req);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponseDTO.success(HttpStatus.CREATED.value(), "대여 요청 성공", res));
+    }
+
+    @PutMapping("/{id}/accept")
+    public ResponseEntity<?> accept(@PathVariable Long id,
+            @AuthenticationPrincipal CustomUserDetails user) {
+        AgreementResponseDTO res = agreementService.accept(user.getId(), id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponseDTO.success(HttpStatus.OK.value(), "대여 승인 성공", res));
     }
 
 }
