7b16eca0cfa91fad9d51ea6ee14438b2d8682c95
Rege-97 <cogus1027@gmail.com>
2025-08-24 20:32:48 +0900

    :sparkles: feat: 대여 목록 조회를 위한 매퍼 작성

--- itjima-server/src/main/java/com/itjima_server/mapper/AgreementMapper.java
+++ itjima-server/src/main/java/com/itjima_server/mapper/AgreementMapper.java
@@ -1,8 +1,11 @@
 package com.itjima_server.mapper;
 
 import com.itjima_server.domain.Agreement;
+import com.itjima_server.domain.AgreementPartyRole;
 import com.itjima_server.domain.AgreementStatus;
+import com.itjima_server.domain.Item;
 import com.itjima_server.dto.agreement.response.AgreementDetailDTO;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -13,8 +16,12 @@ public interface AgreementMapper {
 
     Agreement findById(@Param("id") Long id);
 
-    int updateStatusById(@Param("id")Long id, @Param("status") AgreementStatus status);
+    int updateStatusById(@Param("id") Long id, @Param("status") AgreementStatus status);
 
     AgreementDetailDTO findDetailById(@Param("id") Long id);
+
+    List<Item> findByUserId(@Param("userId") Long userId, @Param("role") AgreementPartyRole role,
+            @Param("lastId") Long lastId,
+            @Param("sizePlusOne") int sizePlusOne);
 }
 
