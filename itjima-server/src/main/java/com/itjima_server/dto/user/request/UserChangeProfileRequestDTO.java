febd3369c8bb01be9cfdfd0b624f400217b65fdd
Rege-97 <cogus1027@gmail.com>
2025-08-28 00:04:11 +0900

    :sparkles: feat: 비밀번호 변경 시 현재 비밀번호 확인 로직 추가

--- itjima-server/src/main/java/com/itjima_server/dto/user/request/UserChangeProfileRequestDTO.java
+++ itjima-server/src/main/java/com/itjima_server/dto/user/request/UserChangeProfileRequestDTO.java
@@ -15,11 +15,18 @@ public class UserChangeProfileRequestDTO {
     @Pattern(regexp = "^\\d{10,11}$", message = "전화번호는 숫자 10~11자리여야 합니다.")
     private String phone;
 
-    @Schema(description = "비밀번호 (영문, 숫자, 특수문자 포함 8~64자)", example = "Test1234!")
+    @Schema(description = "현재 비밀번호 (영문, 숫자, 특수문자 포함 8~64자)", example = "Test1234!")
     @Size(min = 8, max = 64, message = "비밀번호는 8~64자여야 합니다.")
     @Pattern(
             regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=]).{8,64}$",
             message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.")
-    private String password;
+    private String currentPassword;
+
+    @Schema(description = "새 비밀번호 (영문, 숫자, 특수문자 포함 8~64자)", example = "Test1234!")
+    @Size(min = 8, max = 64, message = "비밀번호는 8~64자여야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=]).{8,64}$",
+            message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.")
+    private String newPassword;
 
 }
